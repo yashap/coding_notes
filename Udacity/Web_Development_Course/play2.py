4ef64b39f1165181f93056f5074d4df90094fd44
yashap <ypodeswa@gmail.com>
2014-03-14 14:30:57 -0700

    Added notes on getting location by IP

--- Udacity/Web_Development_Course/play2.py
+++ Udacity/Web_Development_Course/play2.py
@@ -209,4 +209,77 @@ print(x)
 # 	Note that it was also smart enough to convert ' to "
 
 
+############
+# Adding a feature to ASCII chan
+############
 
+import os
+import webapp2
+import jinja2
+import urllib2
+from xml.dom import minidom
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+	autoescape = True)
+
+IP_URL = "http://api.hostip.info/?ip="
+def get_coords(ip):
+	url = IP_URL + ip
+	content = None
+	try:
+		content = urllib2.urlopen(url).read()
+	except URLError:
+		return None
+
+	if content:
+		x = minidom.parseString(xml)
+		coords = x.getElementsByTagName("gml:coordinates")
+		if coords and coords[0].childNodes[0].nodeValue:
+			lon, lat = coords[0].childNodes[0].nodeValue.split(",")
+			return lat, lon
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class Art(db.Model):
+	title = db.StringProperty(required = True)
+	art = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+	def render_front(self, title="", art="", error=""):
+		arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+		self.render("front.html", title=title, art=art, error=error, arts=arts)
+
+	def get(self):
+		self.render_front()
+
+	def post(self):
+		title = self.request.get("title")
+		art = self.request.get("art")
+
+		if title and art:
+			a = Art(title = title, art = art)
+			# lookup user's coordinates from the IP
+			# if they have coords, at them to the Art
+
+			a.put()
+
+			self.redirect("/")
+		else:
+			error = "we need both a title and some artwork!"
+			self.render_front(title, art, error)
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+	debug=True)
